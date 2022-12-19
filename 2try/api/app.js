ed1de847f8b7752b4604de04cec22c08d2aad220
VONHALLER1999 <62394387+VONHALLER1999@users.noreply.github.com>
2022-12-19 20:55:01 +0100

    comments and done with this version

--- 2try/api/app.js
+++ 2try/api/app.js
@@ -1,3 +1,5 @@
+
+//imports of modules
 const express = require("express"); 
 const path=require('path')
 const sqlite3 = require("sqlite3").verbose();
@@ -6,13 +8,12 @@ const app = express();
 const bodyParser=require('body-parser')
 const session = require("express-session");
 var bcrypt = require("bcrypt");
-
 const fs = require("fs");
 const https =require("https")
 
+//creates the https server with the private key and the selfsigned certifcate
 const key = fs.readFileSync("./key.pem");
 const cert = fs.readFileSync("./cert.pem");
-
 const server = https.createServer({ key: key, cert: cert }, app);
 
 server.listen(PORT, () => console.log(`Server lytter på port ${PORT}`));
@@ -21,21 +22,22 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, "/public")));
 ;
-
+//the app uses secure session 
 app.use(
   session({
     secret: "secret",
     resave: true,
     saveUninitialized: true,
+    cookie: {secure: true}
   })
 );
 
-let db = new sqlite3.Database("./userDB.sqlite", (err) => {
+//Makes the database
+let db = new sqlite3.Database("./database/DB.sqlite", (err) => {
     if (err) {
-    // Cannot open database
+    //If the app cannot open database
     throw err;
   } else {
-    var salt = bcrypt.genSaltSync(10);
     db.run(
       `CREATE TABLE Users (
             Id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -53,44 +55,13 @@ let db = new sqlite3.Database("./userDB.sqlite", (err) => {
         } else {
           // Table just created, creating some rows
           console.log("table being created");
-          var insert =
-            "INSERT INTO Users (Username, Email, Password, Salt) VALUES (?,?,?,?)";
-          db.run(insert, [
-            "user1",
-            "user1@example.com",
-            bcrypt.hashSync("user1", salt),
-            salt,
-            Date("now"),
-          ]);
-          console.log("hey");
-          db.run(insert, [
-            "user2",
-            "user2@example.com",
-            bcrypt.hashSync("user2", salt),
-            salt,
-            Date("now"),
-          ]);
-          db.run(insert, [
-            "user3",
-            "user3@example.com",
-            bcrypt.hashSync("user3", salt),
-            salt,
-            Date("now"),
-          ]);
-          db.run(insert, [
-            "user4",
-            "user4@example.com",
-            bcrypt.hashSync("user4", salt),
-            salt,
-            Date("now"),
-          ]);
-          return;
         }
       }
     );
   }
 });
 
+//endpoint with that send data on if the user is logged in or not
 app.get("/loggedstatus", async (req, res) => {
   console.log(req.session)
   if (req.session.loggedIn) {
@@ -99,7 +70,11 @@ app.get("/loggedstatus", async (req, res) => {
     res.send(false);
   }
 });
-
+//send the CSS
+app.get("/style", (req, res) => {
+  res.sendFile(__dirname + "public/style.css");
+})
+//send the client either to home or to the signup page depended on whether the client is logged in
 app.get("/", async (req,res)=>{
     if(req.session.loggedIn){
         console.log("logged in");
@@ -109,10 +84,6 @@ app.get("/", async (req,res)=>{
         res.sendFile(path.join(__dirname, "/public/login-signup.html"));
     }
 })
-app.get("/style", (req, res) => {
-  res.sendFile(__dirname + "public/style.css");
-});
-
 app.get("/home", async (req, res) => {
   if (req.session.loggedIn) {
     res.sendFile(path.join(__dirname, "/public/home.html"));
@@ -122,38 +93,42 @@ app.get("/home", async (req, res) => {
   }
 });
 
+//LOGIN
 app.post("/api/login", async (req, res) => {
-  console.log(req.body)
   try {
+    //Gets the login data from the client
     console.log("trying to log in");
     const Email = req.body.Email;
     const Password = req.body.Password;
   
-    // Make sure there is an Email and Password in the request
+    //Makes sure the client both send an Email and Password in the request
     if (!(Email && Password)) {
       res.status(400).send("All input is required");
       return;
     }
-
+    //checks if the clients data against the database
     let user = [];
     var sql = "SELECT * FROM Users WHERE Email = ?";
-
-    //if something goes with the SQL database else push the user to the array
-    //let result= 
     db.all(sql, Email, function (err, rows) {
       if (err) {
         res.status(400).json({ error: err.message });
         return;
+        //If the user can't be found
+      } else if (rows === []) {
+        console.log("user does not exits");
+        res.status(400).send("No Match");
+        return;
+        //User found, pushes user to array to check password
       } else {
+        console.log(rows);
         rows.forEach(function (row) {
           user.push(row);
-        });
+        })
       }
-
-      //check if the password matches with the hashed password with salt
+      //Checks if the password matches with the hashed password with salt
       var PHash = bcrypt.hashSync(Password, user[0].Salt);
-
       if (PHash === user[0].Password) {
+        //User is found, sets session to true and sends the client to the chatroom
         req.session.loggedIn = true;
         console.log('succesfully logged in');
         res.sendFile(path.join(__dirname, "/public/home.html"));
@@ -166,27 +141,23 @@ app.post("/api/login", async (req, res) => {
   }
 });
 
+//SIGNUP
 app.post("/api/signup", async (req, res) => {
-  var errors = [];
+  console.log('somebody is trying to log in')
   try {
     const { Username, Email, Password } = req.body;
-    if (!Username) {
-      errors.push("Username is missing");
+    if (!Username || !Email|| ! Password) {
+      console.log("Some input is missing");
+      res.status(400);
     }
-    if (!Email) {
-      errors.push("Email is missing");
-    }
-    if (errors.length) {
-      res.status(400).json({ error: errors.join(",") });
-      return;
-    }
-    let userExists = false;
+    //Does the email already exist?
     var sql = "SELECT * FROM Users WHERE Email = ?";
     await db.all(sql, Email, (err, result) => {
       if (err) {
         res.status(402).json({ error: err.message });
         return;
       } 
+      //If the Email isn't occupied save the user in the database
       if (result.length === 0) {
         var salt = bcrypt.genSaltSync(10);
         var data = {
@@ -196,7 +167,6 @@ app.post("/api/signup", async (req, res) => {
           Salt: salt,
           DateCreated: Date("now"),
         };
-
         var sql =
           "INSERT INTO Users (Username, Email, Password, Salt, DateCreated) VALUES (?,?,?,?,?)";
         var params = [
@@ -206,31 +176,40 @@ app.post("/api/signup", async (req, res) => {
           data.Salt,
           Date("now"),
         ];
-
-        var user = db.run(sql, params, function (err, innerResult) {
+        //saves the user in the database
+        db.run(sql, params, function (err) {
+          //error handling
           if (err) {
               console.log(err);
             res.status(400).json({ error: err.message });
             return;
+            //when done saving in the database send the login html file
+          } else {
+            res.sendFile(path.join(__dirname, "/public/login-signup.html"));
           }
         });
+        //if the email is occupied send the sign-up file to the client
       } else {
-        userExists = true;
         res.status(404).sendFile(path.join(__dirname, "/public/login-signup.html"));
           }
     });
+    //error handling
     } catch (err) {
     console.log(err);
     }
 });
 
+//LOGOUT 
+//sets the session to false and send the client to the login/signup page
 app.post("/api/logout", async (req, res) => {
   req.session.loggedIn = false;
   res.sendFile(path.join(__dirname, "/public/login-signup.html"));
 });
 
+//all messages is saved here
 var msgTabel = []
 
+//Gets the messages from the clients and pushes it to the msgTabel
 app.post("/api/getmessage", async (req,res) =>{
   console.log('the body is: ', req.body)
   let msg = req.body.msg
@@ -238,6 +217,7 @@ app.post("/api/getmessage", async (req,res) =>{
   res.sendFile(path.join(__dirname, "/public/home.html"));
 })
 
+//send the current msgtabel
 app.get("/api/currentchat", async (req, res) => {
   console.log('should sent msgtabel:',msgTabel)
   res.send(msgTabel)
--- 2try/api/public/index.js
+++ 2try/api/public/index.js
@@ -1,22 +1,53 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   event.preventDefault();
 
+    //when the DOM is loaded fetch the current messages from the backend
+    loadchat()
 
-    fetch("https://localhost:3000/api/currentchat")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data.length);
-        createTable(data)
+    function loadchat(){
+      fetch("https://localhost:3000/api/currentchat")
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data.length);
+          createTable(data);
+        });
+    }
+      //refreshes the chat
+      document.getElementById('refresh').addEventListener('click', (e)=>{
+         event.preventDefault();
+        console.log('refreshing the chat')
+        loadchat()
       })
-       
+
+      //When a message is sent also update the chat
+      document.getElementById("sendMsg").addEventListener("click", (e) => {
+        event.preventDefault()
+        console.log("sent a msg and refreshing the chat");
+        loadchat();
+      });
+
+      //function for putting the messages in the chatroom
+      el = document.getElementById('myPar')
       function createTable(data) {
+        let pElements = document.querySelectorAll("p");
+        for (let i = 1; i < pElements.length; i++) {
+          pElements[i].parentNode.removeChild(pElements[i]);
+        }
         for (let i = 0; i < data.length; i++) {
+
+          let para = document.createElement("p")
+          let node = document.createTextNode(data[i])
+          para.appendChild(node)
+          el.appendChild(para)
+
+          /*
           var table = document.getElementById("table");
           var row = table.insertRow(i);
 
           var cell1 = row.insertCell(i);
 
           cell1.innerHTML = data;
+          */
         }
       }
     
